37805d4b6e555e5d2442e6f54c8d46851035b4b5
Joshua Sy Go <joshsygobusiness@gmail.com>
2021-09-13 14:34:03 -0700

    defined submit button event handler

--- data.js
+++ data.js
@@ -1,49 +1,61 @@
-var data = [{
-  day: 'Monday',
-  description: null,
-  time: null,
-  nextEntryId: 1,
-  editing: null
-},
-{
-  day: 'Tuesday',
-  description: null,
-  time: null,
-  nextEntryId: 1,
-  editing: null
-},
-{
-  day: 'Wednesday',
-  description: null,
-  time: null,
-  nextEntryId: 1,
-  editing: null
-},
-{
-  day: 'Thursday',
-  description: null,
-  time: null,
-  nextEntryId: 1,
-  editing: null
-},
-{
-  day: 'Friday',
-  description: null,
-  time: null,
-  nextEntryId: 1,
-  editing: null
-},
-{
-  day: 'Saturday',
-  description: null,
-  time: null,
-  nextEntryId: 1,
-  editing: null
-},
-{
-  day: 'Sunday',
-  description: null,
-  time: null,
-  nextEntryId: 1,
-  editing: null
-}];
+// var data = [{
+//   day: 'Monday',
+//   description: null,
+//   time: null,
+//   nextEntryId: 1,
+//   editing: null
+// },
+// {
+//   day: 'Tuesday',
+//   description: null,
+//   time: null,
+//   nextEntryId: 1,
+//   editing: null
+// },
+// {
+//   day: 'Wednesday',
+//   description: null,
+//   time: null,
+//   nextEntryId: 1,
+//   editing: null
+// },
+// {
+//   day: 'Thursday',
+//   description: null,
+//   time: null,
+//   nextEntryId: 1,
+//   editing: null
+// },
+// {
+//   day: 'Friday',
+//   description: null,
+//   time: null,
+//   nextEntryId: 1,
+//   editing: null
+// },
+// {
+//   day: 'Saturday',
+//   description: null,
+//   time: null,
+//   nextEntryId: 1,
+//   editing: null
+// },
+// {
+//   day: 'Sunday',
+//   description: null,
+//   time: null,
+//   nextEntryId: 1,
+//   editing: null
+// }];
+
+var data = {
+  sunday: [],
+  monday: [],
+  tuesday: [],
+  wednesday: [],
+  thursday: [],
+  friday: [],
+  saturday: [],
+  editing: null,
+  nextEntryId: 1
+};
--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+var data = {
+  sunday: [],
+  monday: [],
+  tuesday: [],
+  wednesday: [],
+  thursday: [],
+  friday: [],
+  saturday: [],
+  editing: null,
+  nextEntryId: 1
+};
+
 var $entryForm = document.querySelector('form');
+var $modalOverlay = document.querySelector('.overlay');
 // var $submit = document.querySelectorAll('a');
-var $submit = document.querySelector('#submit');
+// var $submit = document.querySelector('#submit');
 
 // $submit.addEventListener('click', handleSubmit);
 
@@ -9,12 +22,28 @@ var $submit = document.querySelector('#submit');
 // }
 
 function formSubmitHandler(event) {
+  event.preventDefault();
   console.log('form submit');
   var entry = {
     day: $entryForm.elements.day.value,
     time: $entryForm.elements.day.value,
     description: $entryForm.elements.day.value
+    // entryId: data.nextEntryId
   };
+
+  entry.entryId = data.nextEntryId;
+  data.nextEntryId++;
+
+  for (var day in data) {
+    // debugger;
+    if (day === entry.day) {
+      data[day].unshift(entry);
+    }
+  }
+
+  $modalOverlay.setAttribute('class', 'hidden');
+  console.log('data:', data);
+
   console.log('entry', entry);
   // if (!event.target.matches('#submit')) {
   //   return
